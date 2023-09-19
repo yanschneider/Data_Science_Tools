ce541dde39badecca7f5e498aa2484a5757fab4d
Yannic Schneider <yannicschneider@me.com>
2023-09-19 11:43:12 +0200

    predicting values with linear regression

--- 2_Regression/1_Simple_Linear_Regression/simple_linear_regression.py
+++ 2_Regression/1_Simple_Linear_Regression/simple_linear_regression.py
@@ -4,14 +4,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 # Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv',delimiter=',')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Training the Simple Linear Regression model on the Training set
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
+
+y_pred = regressor.predict(X_test)
+
+
 
 # Visualising the Training set results
 
