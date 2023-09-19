abac643ba0b5696fe647e6b432e07eedc8ad71b4
Yannic Schneider <yannicschneider@me.com>
2023-09-19 11:19:56 +0200

    init linear regression

---
+++ 2_Regression/1_Simple_Linear_Regression/simple_linear_regression.py
@@ -0,0 +1,18 @@
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+# Splitting the dataset into the Training set and Test set
+
+# Training the Simple Linear Regression model on the Training set
+
+# Predicting the Test set results
+
+# Visualising the Training set results
+
+# Visualising the Test set results
