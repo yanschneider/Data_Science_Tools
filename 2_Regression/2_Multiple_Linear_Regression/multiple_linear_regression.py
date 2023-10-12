3b266629a916fb278c8cad15f60d7c169f55b422
Yannic Schneider <yannicschneider@me.com>
2023-10-12 16:12:34 +0200

    no comment

---
+++ 2_Regression/2_Multiple_Linear_Regression/multiple_linear_regression.py
@@ -0,0 +1,22 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+
+# Encoding Dataset
+
+# Splitting dataset into training and test set
+
+# Training the muliple linear regression model on the training set
+
+# predicting test set results
+
+
